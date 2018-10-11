b1ead3d8cb89afb7d50987765a0274db18708af1
Mark Henwood <mcbhenwood@gmail.com>
2018-10-11 15:05:04 +0100

    Set up shared ES domain

---
+++ infrastructure/es.tf
@@ -0,0 +1,49 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_elasticsearch_domain" "es" {
+  domain_name           = "${var.domain}"
+  elasticsearch_version = "6.2"
+
+  cluster_config {
+    instance_type = "t2.small.elasticsearch"
+    instance_count = 1
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = "10"
+  }
+
+  advanced_options {
+    "rest.action.multi.allow_explicit_index" = "true"
+  }
+
+  access_policies = <<CONFIG
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "es:*",
+      "Resource": "arn:aws:es:${var.aws_region}:${data.aws_caller_identity.current.account_id}:domain/${var.domain}/*",
+      "Condition": {
+        "IpAddress": {
+          "aws:SourceIp": "${var.safe_ip_address}"
+        }
+      }
+    }
+  ]
+}
+CONFIG
+
+  snapshot_options {
+    automated_snapshot_start_hour = 23
+  }
+
+  tags {
+    Domain = "NHSDHackday2018MH"
+  }
+}
