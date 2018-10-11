1b90df71141e5b91f6f2e61e7233e37c67d0120b
Mark Henwood <mcbhenwood@gmail.com>
2018-10-11 15:04:53 +0100

    Set up terraform basics

---
+++ infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "domain" {
+  default = "nhsd-hackday2018-mh"
+}
+
+variable "aws_region" {
+  default = "eu-west-2"
+}
+
+variable "safe_ip_address" {
+  default = "62.253.231.2"
+}
+
+variable "aws_profile" {
+  default = "mcbh"
+}
