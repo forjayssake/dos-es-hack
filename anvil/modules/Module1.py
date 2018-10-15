e2044ff3652e8dba4a165cecaaf83813a3153c6e
George Jacob <george.jacob@bjss.com>
2018-10-15 09:49:23 +0100

    Anvil UI code to here

---
+++ anvil/modules/Module1.py
@@ -0,0 +1,13 @@
+import anvil.server
+import anvil.http
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example:
+#
+#    import Module1
+#
+#    Module1.say_hello()
+#
+
+def say_hello():
+  print "Hello, world"
+  
